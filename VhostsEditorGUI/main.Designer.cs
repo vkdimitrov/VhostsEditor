32dfce4f4485616fcb1e5d9f57a6f610da7bb2b5
vkdimitrov <dimitrov@vladko.org>
2012-05-20 23:06:10 +0300

    GUI v0.1

---
+++ VhostsEditorGUI/main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace VhostsEditorGUI
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Add = new System.Windows.Forms.Button();
+            this.Edit = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.ResetApache = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ServerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DocumentRoot = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(352, 44);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(83, 23);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            // 
+            // Edit
+            // 
+            this.Edit.Location = new System.Drawing.Point(352, 73);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(83, 23);
+            this.Edit.TabIndex = 1;
+            this.Edit.Text = "Edit";
+            this.Edit.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(352, 102);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(83, 23);
+            this.Delete.TabIndex = 2;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // ResetApache
+            // 
+            this.ResetApache.Location = new System.Drawing.Point(352, 151);
+            this.ResetApache.Name = "ResetApache";
+            this.ResetApache.Size = new System.Drawing.Size(83, 23);
+            this.ResetApache.TabIndex = 3;
+            this.ResetApache.Text = "ResetApache";
+            this.ResetApache.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ServerName,
+            this.DocumentRoot});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(334, 162);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // ServerName
+            // 
+            this.ServerName.HeaderText = "ServerName";
+            this.ServerName.Name = "ServerName";
+            // 
+            // DocumentRoot
+            // 
+            this.DocumentRoot.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DocumentRoot.HeaderText = "DocumentRoot";
+            this.DocumentRoot.Name = "DocumentRoot";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 196);
+            this.Controls.Add(this.ResetApache);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.Add);
+            this.Name = "main";
+            this.Text = "VhostsEditor 0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Edit;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button ResetApache;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ServerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DocumentRoot;
+    }
+}
+
